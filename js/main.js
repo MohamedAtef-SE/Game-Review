1eaaebbd8e58918dfa7486155dccf1823b0d1bd8
mohamedatef-SE <mohamedatefyoussef@gmail.com>
2024-01-22 10:44:23 +0200

    activeButton() added

--- js/main.js
+++ js/main.js
@@ -11,9 +11,28 @@ fetchAPI(url);
 
 
 const navBtns = document.querySelectorAll('.navbar-nav .nav-item');
+
+
+function activeButton(button) {
+    //Decative all buttons
+    navBtns.forEach((btn) => {
+        btn.classList.remove('active');
+    })
+    //Adding active class on clicked button only.
+    button.classList.add('active');
+}
+
+
 navBtns.forEach((btn) => {
     btn.addEventListener('click', function (e) {
         const url = `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${e.target.innerText.toLowerCase()}`;
         fetchAPI(url);
+        activeButton(e.target);
     })
+
+    //OR
+    // btn.addEventListener('click',activeButton );
 });
+
+
+
